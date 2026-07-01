74ea052d2b3d1b4a71a2d44499d4cf4ca71c6b22
shubhmittal8971 <shubhmittal8971@gmail.com>
2026-07-01 10:16:14 +0000

    DAY366

--- server/models/Log.js
+++ server/models/Log.js
@@ -1,9 +1,9 @@
 import mongoose from 'mongoose';
 
 const LogSchema = new mongoose.Schema({
+  level: { type: String, required: true },
+  message: { type: String, required: true },
   timestamp: { type: Date, default: Date.now },
-  event: { type: String, required: true },
-  origin: { type: String, required: true },
 });
 
 const Log = mongoose.model('Log', LogSchema);
