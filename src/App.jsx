2656414a8bf702131878e492088b4f21fde97f49
shubhmittal8971 <shubhmittal8971@gmail.com>
2026-07-04 19:14:26 +0000

    DAY401

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   return (
     <>
-      <Header />
+      <Header logo={reactLogo} />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import './Header.css';
 
-const Header = () => {
+const Header = ({ logo }) => {
   return (
     <header className="header">
+      {logo && <img src={logo} className="header-logo" alt="logo" />}
       <h1>My Awesome App</h1>
     </header>
   );
