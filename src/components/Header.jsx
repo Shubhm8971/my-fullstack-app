ac5b1120d5ae773c258a80ecd2f31070dc168d60
shubhmittal8971 <shubhmittal8971@gmail.com>
2026-07-05 11:09:06 +0000

    DAY411

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const Header = ({ logo }) => {
   return (
     <header className="header">
       {logo && <img src={logo} className="header-logo" alt="logo" />}
-      <h1>My Awesome App</h1>
+      <h1>Stellar App</h1>
     </header>
   );
 };
