dcb3a7ce4b19722c8267b30f9789a0a0ee138f10
shubhmittal8971 <shubhmittal8971@gmail.com>
2026-07-13 07:09:20 +0000

    DAY491

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './Footer.css';
+import { FaGithub, FaTwitter, FaLinkedin } from 'react-icons/fa';
 
 const Footer = () => {
   return (
     <footer className="footer">
       <p>&copy; {new Date().getFullYear()} Stellar App. All rights reserved.</p>
       <div className="social-media">
-        <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">Facebook</a>
-        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">Twitter</a>
-        <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+        <a href="https://github.com" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+        <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
+        <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
       </div>
     </footer>
   );
